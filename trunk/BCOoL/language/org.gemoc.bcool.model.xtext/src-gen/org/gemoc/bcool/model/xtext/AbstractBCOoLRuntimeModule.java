aa93dd66ed3b025af7cac7ed01d388a2e85f94a1
jdeanton <jdeanton@b8474f6b-9d94-455f-b200-add8ad8c1022>
2014-12-02 10:08:19 +0000

    not working well version: need to clarify the type of the attribute from the type of the feature...

--- trunk/BCOoL/language/org.gemoc.bcool.model.xtext/src-gen/org/gemoc/bcool/model/xtext/AbstractBCOoLRuntimeModule.java
+++ trunk/BCOoL/language/org.gemoc.bcool.model.xtext/src-gen/org/gemoc/bcool/model/xtext/AbstractBCOoLRuntimeModule.java
@@ -94,11 +94,6 @@ public abstract class AbstractBCOoLRuntimeModule extends DefaultRuntimeModule {
 		return org.gemoc.bcool.model.xtext.validation.BCOoLValidator.class;
 	}
 
-	// contributed by org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public void configureIgnoreCaseLinking(com.google.inject.Binder binder) {
-		binder.bindConstant().annotatedWith(org.eclipse.xtext.scoping.IgnoreCaseLinking.class).to(false);
-	}
-
 	// contributed by org.eclipse.xtext.generator.builder.BuilderIntegrationFragment
 	public Class<? extends org.eclipse.xtext.resource.IContainer.Manager> bindIContainer$Manager() {
 		return org.eclipse.xtext.resource.containers.StateBasedContainerManager.class;
