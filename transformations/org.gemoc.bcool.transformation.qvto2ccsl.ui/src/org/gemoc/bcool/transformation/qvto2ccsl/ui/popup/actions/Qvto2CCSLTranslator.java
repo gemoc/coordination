3a694d0ea7f4b27b4a198f747bee7d3e1b0a4183
mvaralar <matiasevara@gmail.com>
2015-05-13 13:58:24 +0000

    Full supporting of transfo between two lenguages

--- transformations/org.gemoc.bcool.transformation.qvto2ccsl.ui/src/org/gemoc/bcool/transformation/qvto2ccsl/ui/popup/actions/Qvto2CCSLTranslator.java
+++ transformations/org.gemoc.bcool.transformation.qvto2ccsl.ui/src/org/gemoc/bcool/transformation/qvto2ccsl/ui/popup/actions/Qvto2CCSLTranslator.java
@@ -1,16 +1,22 @@
 package org.gemoc.bcool.transformation.qvto2ccsl.ui.popup.actions;
 
+import java.awt.List;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.m2m.internal.qvt.oml.expressions.ModelParameter;
+import org.eclipse.m2m.internal.qvt.oml.expressions.OperationalTransformation;
 import org.eclipse.m2m.qvt.oml.BasicModelExtent;
 import org.eclipse.m2m.qvt.oml.ExecutionContextImpl;
 import org.eclipse.m2m.qvt.oml.ExecutionDiagnostic;
@@ -35,6 +41,10 @@ public class Qvto2CCSLTranslator implements IObjectActionDelegate {
 	private IFile qvtoFile=null;
 	private XtextResourceSet aModelResourceSet=null;
 	private XtextResourceSet outputResourceSet=null;
+	
+	
+	private ArrayList<IFile> modelfiles = new ArrayList<IFile>();
+	
 	
 	/**
 	 * Constructor for Action1.
@@ -73,77 +83,110 @@ public class Qvto2CCSLTranslator implements IObjectActionDelegate {
 		String qvtoUriString = qvtoFile.getLocation().toOSString();
 	    URI transformationURI = URI.createFileURI(qvtoUriString);
 	    
+	    // output file
+	    String qvtUriString0 = qvtoFile.getProject().getName()+"/"+qvtoFile.getProjectRelativePath().toOSString();
+	    int numberOfCharToRemove1 = qvtUriString0.length() - transformationURI.fileExtension().length() -1;
+	    String outputPathStringtotal = qvtUriString0.substring(0, numberOfCharToRemove1)+"Coordinated.extendedCCSL";
 		
-	    //model1 resource
-	    String modelUriString1 = modelFile1.getProject().getName()+"/"+modelFile1.getProjectRelativePath().toOSString();
-	    URI modelUri1 = URI.createPlatformResourceURI(modelUriString1,false);
-	    Resource modelResource1 = aModelResourceSet.getResource(modelUri1, true);
-	   
-	    HashMap<Object, Object> saveOptions1 = new HashMap<Object, Object>();
-	    Builder aBuilder1 = SaveOptions.newBuilder();
-	    SaveOptions anOption1 = aBuilder1.getOptions();
-	    anOption1.addTo(saveOptions1);
-	    try {
-	    	modelResource1.load(saveOptions1);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-	  //model2 resource
-	    String modelUriString2 = modelFile2.getProject().getName()+"/"+modelFile2.getProjectRelativePath().toOSString();
-	    URI modelUri2 = URI.createPlatformResourceURI(modelUriString2,false);
-	    Resource modelResource2 = aModelResourceSet.getResource(modelUri2, true);
-	   
-	    HashMap<Object, Object> saveOptions2 = new HashMap<Object, Object>();
-	    Builder aBuilder2 = SaveOptions.newBuilder();
-	    SaveOptions anOption2 = aBuilder2.getOptions();
-	    anOption2.addTo(saveOptions2);
-	    try {
-	    	modelResource2.load(saveOptions2);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	    
-	   
-	    //transformationURI is the URI of qvto file
-		TransformationExecutor executor = new TransformationExecutor(transformationURI);
-
-		//inResource1 have the vaule of inModel
-		ModelExtent input1 = new BasicModelExtent(modelResource1.getContents());
-		ModelExtent input2 = new BasicModelExtent(modelResource2.getContents());
-		ModelExtent output = new BasicModelExtent();
-
-		ExecutionContextImpl context = new ExecutionContextImpl();
-		// importa el orden!!!!!
-		ExecutionDiagnostic diagnostic = executor.execute(context, input2, input1, output);
-		System.out.println(diagnostic);
-		//output resource saving
-		int numberOfCharToRemove1 = modelUriString1.length() - modelUri1.fileExtension().length() -1;
-	    String outputPathString = modelUriString1.substring(0, numberOfCharToRemove1)+ "_coordinatedWith_";
-		int numberOfCharToRemove2 = modelUriString2.length() - modelUri2.fileExtension().length() -1;
-	    String tmp = modelUriString2.substring(0, numberOfCharToRemove2);
-	    tmp = tmp.substring(tmp.lastIndexOf('/')+1, tmp.length()-1)+"_MoCC.extendedCCSL";
-	    outputPathString += tmp;
-	    
-	    URI outputUri = URI.createPlatformResourceURI(outputPathString,false);
-	    Resource outputResource=null;
+	    URI outputUritotal = URI.createPlatformResourceURI(outputPathStringtotal,false);
+	    Resource outputResourcetotal=null;
 	    try{
-	    	outputResource = outputResourceSet.createResource(outputUri);
+	    	outputResourcetotal = outputResourceSet.createResource(outputUritotal);
 	    }catch( Exception e){
 	    	System.out.println(e);
-	    	outputResource = outputResourceSet.createResource(outputUri);
-
-	    //	outputResource = outputResourceSet.getResource(outputUri,true);
+	    	outputResourcetotal = outputResourceSet.createResource(outputUritotal);
 	    };
-	    outputResource.getContents().addAll(output.getContents());
-	    try {
-			outputResource.save(null);
+	    
+	    //executor and context
+	    TransformationExecutor executor = new TransformationExecutor(transformationURI);
+	    ExecutionContextImpl context = new ExecutionContextImpl();
+	    ModelExtent output = new BasicModelExtent();
+	    
+	    // trace of the applicacion of the operators
+	    HashMap<IFile,Boolean> tracemodels = new HashMap<IFile, Boolean>();
+	    
+	    // ugly since it should be check the parameters
+		for (IFile model1 : modelfiles) {
+		    //model1 resource
+		    String model1UriString = model1.getProject().getName()+"/"+model1.getProjectRelativePath().toOSString();
+		    URI model1Uri = URI.createPlatformResourceURI(model1UriString,false);
+		    Resource model1Resource = aModelResourceSet.getResource(model1Uri, true);
+		   
+		    HashMap<Object, Object> saveOptions1 = new HashMap<Object, Object>();
+		    Builder aBuilder1 = SaveOptions.newBuilder();
+		    SaveOptions anOption1 = aBuilder1.getOptions();
+		    anOption1.addTo(saveOptions1);
+		    try {
+		    	model1Resource.load(saveOptions1);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		    ModelExtent input1 = new BasicModelExtent(model1Resource.getContents());
+		    
+		    //tracemodels.put(model1, true);
+			//////////////////////////////////////////////////////////////////////////////
+			for (IFile model2 : modelfiles){
+				
+				//model2 resource
+			    String model2UriString = model2.getProject().getName()+"/"+model2.getProjectRelativePath().toOSString();
+			    URI model2Uri = URI.createPlatformResourceURI(model2UriString,false);
+			    Resource model2Resource = aModelResourceSet.getResource(model2Uri, true);
+				
+			    
+				if ((model1.getFullPath().toString().equals(model2.getFullPath().toString())))  {
+				}else{
+				
+				    // models conforming to same language, the transfo is aplicable only one time
+				    if  ((model2Uri.fileExtension().equals(model1Uri.fileExtension())) && (tracemodels.get(model2) == null)) {  
+				  
+				    	tracemodels.put(model1,true);
+				    	
+				    	HashMap<Object, Object> saveOptions2 = new HashMap<Object, Object>();
+				    	Builder aBuilder2 = SaveOptions.newBuilder();
+				    	SaveOptions anOption2 = aBuilder2.getOptions();
+				    	anOption2.addTo(saveOptions2);
+				    	try {
+				    		model2Resource.load(saveOptions2);
+				    	} catch (IOException e1) {
+				    		// TODO Auto-generated catch block
+				    		e1.printStackTrace();
+				    	}
+				    
+				    	ModelExtent input2 = new BasicModelExtent(model2Resource.getContents());
+				    	ExecutionDiagnostic diagnostic = executor.execute(context, input1, input2 , output);
+				    // models conforming different languages
+				    } else if  (!(model2Uri.fileExtension().equals(model1Uri.fileExtension()))) {
+				    	HashMap<Object, Object> saveOptions2 = new HashMap<Object, Object>();
+				    	Builder aBuilder2 = SaveOptions.newBuilder();
+				    	SaveOptions anOption2 = aBuilder2.getOptions();
+				    	anOption2.addTo(saveOptions2);
+				    	try {
+				    		model2Resource.load(saveOptions2);
+				    	} catch (IOException e1) {
+				    		// TODO Auto-generated catch block
+				    		e1.printStackTrace();
+				    	}
+				    
+				    	ModelExtent input2 = new BasicModelExtent(model2Resource.getContents());
+				    	ExecutionDiagnostic diagnostic = executor.execute(context, input1, input2 , output);
+				    	
+				    }
+					
+				}
+			}
+		}
+		
+		outputResourcetotal.getContents().addAll(output.getContents());
+		
+		try {
+			outputResourcetotal.save(null);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	    
+	 
 
 	}
 
@@ -156,6 +199,8 @@ public class Qvto2CCSLTranslator implements IObjectActionDelegate {
 		qvtoFile=null;
 		modelFile1=null;
 		modelFile2=null;
+		modelfiles.removeAll(modelfiles);
+		
 		if (selection instanceof StructuredSelection) {
 			StructuredSelection ts = (StructuredSelection) selection;
 			Iterator it = ts.iterator();
@@ -171,6 +216,7 @@ public class Qvto2CCSLTranslator implements IObjectActionDelegate {
 						}else{
 							modelFile2 = (IFile) o;
 						}
+						modelfiles.add((IFile) o);
 					}
 					
 					
