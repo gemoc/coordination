23ea513440af05242ae1c5b1843e0cbc3cf83a9b
jdeanton <julien.deantoni@inria.fr>
2015-06-02 14:28:52 +0000

    remove strange stuff

--- examples/org.gemoc.bcool.example.productfumlandtfsm/src-gen/pivot/Annotation.java
+++
@@ -1,9 +0,0 @@
-package pivot;
-
-@SuppressWarnings("all")
-public class Annotation {
-  public java.lang.String toString() {
-    String result = new org.eclipse.xtext.xbase.lib.util.ToStringHelper().toString(this);
-    return result;
-  }
-}
