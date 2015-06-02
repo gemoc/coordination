9b41e5e6af18b763d230bf70c6345320c0b01beb
jdeanton <jdeanton@b8474f6b-9d94-455f-b200-add8ad8c1022>
2015-06-02 14:28:52 +0000

    remove strange stuff

--- trunk/BCOoL/examples/org.gemoc.bcool.example.productfumlandtfsm/src-gen/UML/UML/Action/context.java
+++
@@ -1,9 +0,0 @@
-package UML.UML.Action;
-
-@SuppressWarnings("all")
-public class context {
-  public java.lang.String toString() {
-    String result = new org.eclipse.xtext.xbase.lib.util.ToStringHelper().toString(this);
-    return result;
-  }
-}
--- trunk/BCOoL/examples/org.gemoc.bcool.example.productfumlandtfsm/src-gen/UML/UML/Action/output/union.java
+++
@@ -1,9 +0,0 @@
-package UML.UML.Action.output;
-
-@SuppressWarnings("all")
-public class union {
-  public java.lang.String toString() {
-    String result = new org.eclipse.xtext.xbase.lib.util.ToStringHelper().toString(this);
-    return result;
-  }
-}
