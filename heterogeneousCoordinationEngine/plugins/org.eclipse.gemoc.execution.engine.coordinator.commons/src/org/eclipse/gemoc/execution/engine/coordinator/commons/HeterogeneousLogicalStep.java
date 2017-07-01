2a6ce7e0868eb91741a5294ce17f60664e4c7cde
Julien Deantoni <julien.deantoni@inria.fr>
2017-07-01 23:54:17 +0200

    toward correct compiling

--- heterogeneousCoordinationEngine/plugins/org.eclipse.gemoc.execution.engine.coordinator.commons/src/org/eclipse/gemoc/execution/engine/coordinator/commons/HeterogeneousLogicalStep.java
+++ heterogeneousCoordinationEngine/plugins/org.eclipse.gemoc.execution.engine.coordinator.commons/src/org/eclipse/gemoc/execution/engine/coordinator/commons/HeterogeneousLogicalStep.java
@@ -1,33 +1,33 @@
-//package org.eclipse.gemoc.execution.engine.coordinator.commons;
-//
-//import java.util.ArrayList;
-//
-//import org.eclipse.gemoc.execution.engine.coordinator.commons.HeterogeneousEngine.ExtendedLogicalStep;
-//import org.eclipse.gemoc.trace.commons.model.trace.impl.StepImpl;
-//
-//
-///**
-// * this is a list of integer where each integer is index of the logical step of its solver. 
-// * The index of the integer itself represents the associated solver index in the _coordinatedEngines list 
-// */
-//public class HeterogeneousLogicalStep extends StepImpl {
-//
-//	public HeterogeneousLogicalStep() {
-//		// TODO Auto-generated constructor stub
-//	}
-//
-//	private static final long serialVersionUID = 7306227694056928033L;
-//	
-//	public ArrayList<ExtendedLogicalStep> logicalSteps = new ArrayList<ExtendedLogicalStep>();
-//	
-//	
-//	/*@Override
-//	public EList<MSEOccurrence> getMseOccurrences(){
-//		EList<MSEOccurrence> res = new BasicEList<MSEOccurrence>(); 
-//		for (LogicalStep l : logicalSteps){
-//			res.addAll(l.getMseOccurrences());
-//		}
-//		return res;
-//	}*/
-//
-//}
+package org.eclipse.gemoc.execution.engine.coordinator.commons;
+
+import java.util.ArrayList;
+
+import org.eclipse.gemoc.execution.engine.coordinator.commons.HeterogeneousEngine.ExtendedLogicalStep;
+import org.eclipse.gemoc.trace.commons.model.trace.impl.StepImpl;
+
+
+/**
+ * this is a list of integer where each integer is index of the logical step of its solver. 
+ * The index of the integer itself represents the associated solver index in the _coordinatedEngines list 
+ */
+public class HeterogeneousLogicalStep extends StepImpl {
+
+	public HeterogeneousLogicalStep() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = 7306227694056928033L;
+	
+	public ArrayList<ExtendedLogicalStep> logicalSteps = new ArrayList<ExtendedLogicalStep>();
+	
+	
+	/*@Override
+	public EList<MSEOccurrence> getMseOccurrences(){
+		EList<MSEOccurrence> res = new BasicEList<MSEOccurrence>(); 
+		for (LogicalStep l : logicalSteps){
+			res.addAll(l.getMseOccurrences());
+		}
+		return res;
+	}*/
+
+}
